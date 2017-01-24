ac3f9a768337c5a6a625656b104a059661ece75d
James Duin <jamesdduin@gmail.com>
2017-01-24 10:35:13 -0600

    add iterate through folds list fcns

--- methods.py
+++ methods.py
@@ -34,3 +34,276 @@ def createFolds(set, folds):
             partitionCounter += 1
             if partitionCounter > 10:
                 partitionCounter = 1
+
+
+def iterateFoldsCoarse(rndNum, coarse_folds):
+    fold_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # fold_list = [1]
+    rnd1_results_coarse = [['fold', 'roc_auc', 'pr_auc']]
+    for testFold in fold_list:
+        print("rnd"+str(rndNum)+"_coarse fold" + str(testFold))
+        ##### Create train set for coarse
+        data = []
+        partition_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        partition_list.remove(testFold)
+        for x in partition_list:
+            # partition = np.loadtxt("../data/partition_"+str(x))
+            # partition = np.loadtxt(coarse_folds[x])
+            partition = np.asarray(coarse_folds[x])
+            if data == []:
+                data = partition
+            else:
+                data = np.vstack((partition, data))
+                # print(str(x)+"=>" +str(data.shape))
+        # print(data.shape)
+        y_train, X_trainPreScale = data[:, 0], data[:, 1:data.shape[1]]
+        # print(y_train)
+        # print(X_trainPreScale)
+        y_trainCoarse = []
+        for i in y_train:
+            if i > 0:
+                y_trainCoarse.append(1.)
+            else:
+                y_trainCoarse.append(i)
+        # print("print(y_trainCoarse)")
+        # print(y_trainCoarse)
+
+        #### scale dataset
+        scaler = preprocessing.StandardScaler().fit(X_trainPreScale);
+        X_trainFull = scaler.transform(X_trainPreScale);
+        selector = SelectPercentile(f_classif, percentile=75);
+        selector.fit(X_trainFull, y_trainCoarse);
+        X_train = selector.transform(X_trainFull);
+
+        ##### Create test set for coarse
+        # data_test = np.loadtxt("../data/partition_" + str(testFold))
+        data_test = np.asarray(coarse_folds[testFold])
+        y_test, X_testPreScale = data_test[:, 0], data_test[:, 1:data_test.shape[1]];
+        X_testFull = scaler.transform(X_testPreScale);
+        X_test = selector.transform(X_testFull);
+        y_testCoarse = []
+        for i in y_test:
+            if i > 0:
+                y_testCoarse.append(1.)
+            else:
+                y_testCoarse.append(i)
+
+        ##### Train classifier for coarse
+        classifier = svm.SVC(C=10.0, kernel='poly', degree=3, probability=True, cache_size=8192,
+                             decision_function_shape='ovr', verbose=False)
+        clf = classifier.fit(X_train, y_trainCoarse)
+        joblib.dump(clf, 'coarse_models/rnd'+str(rndNum)+'_coarse_fold_' + str(testFold) + '.pkl')
+        # clf = joblib.load('coarse_models/rnd1_coarse_fold_'+str(testFold)+'.pkl')
+        y_pred = clf.predict(X_test);
+
+        ##### Predict test set for coarse
+        y_predCoarse = []
+        for i in y_pred:
+            if i > 0:
+                y_predCoarse.append(1.)
+            else:
+                y_predCoarse.append(i)
+        # Compute confusion matrix
+        # cm = confusion_matrix(y_testCoarse, y_predCoarse)
+        # np.set_printoptions(precision=2)
+        # print('Confusion matrix, without normalization')
+        # print(cm)
+        # print('Precision / Recall')
+        # print(precision_score(y_testCoarse, y_predCoarse, average='binary'))
+        # print(recall_score(y_testCoarse, y_predCoarse, average='binary'))
+        y_prob = clf.predict_log_proba(X_test)
+        # print(y_prob.shape)
+        y_score = []
+        for i in range(0, y_prob.shape[0]):
+            # print(y_prob[i,:])
+            maxVal = max(y_prob[i, :])
+            if maxVal == y_prob[i, 0]:
+                maxVal = 0.
+            y_score.append(maxVal - y_prob[i, 0])
+
+        ###### Print this folds roc_auc for coarse
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        fpr[1], tpr[1], thresholds = roc_curve(y_testCoarse, y_score)
+        roc_auc[1] = auc(fpr[1], tpr[1])
+        # Plot of a ROC curve
+        plt.figure()
+        plt.plot(fpr[1], tpr[1], label='ROC curve (area = %0.5f)' % roc_auc[1])
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
+        # plt.show()
+        plt.savefig('results/rnd'+str(rndNum)+'_coarse_ROC_' + str(testFold) + '.png')
+
+        ###### Print this folds pr_auc for coarse
+        precision = dict()
+        recall = dict()
+        average_precision = dict()
+        precision[1], recall[1], _ = precision_recall_curve(y_testCoarse, y_score)
+        average_precision[1] = average_precision_score(y_testCoarse, y_score)
+        # Plot Precision-Recall curve
+        plt.clf()
+        plt.plot(recall[1], precision[1], label='Precision-Recall curve')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.ylim([0.0, 1.05])
+        plt.xlim([0.0, 1.0])
+        plt.title('Precision-Recall: AUC={0:0.5f}'.format(average_precision[1]))
+        plt.legend(loc="lower left")
+        # plt.show()
+        plt.savefig('results/rnd'+str(rndNum)+'_coarse_PR_' + str(testFold) + '.png')
+        rnd1_results_coarse.append([str(testFold)] + [roc_auc[1]] + [average_precision[1]]);
+
+    ###### Save coarse results to a file
+    f = open('results/rnd'+str(rndNum)+'_coarseResults.txt', 'w')
+    for result in rnd1_results_coarse:
+        index = 0
+        for r in result[:-1]:
+            f.write(str(result[index]) + ", ")
+            index += 1
+        f.write(str(result[-1]))
+        f.write("\n")
+    f.close()
+
+
+
+
+
+
+
+
+
+def iterateFoldsFine(rndNum,fine_folds):
+    fold_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # fold_list = [1]
+    rnd1_results_fine = [['fold', 'roc_auc', 'pr_auc']]
+    for testFold in fold_list:
+        print("rnd"+str(rndNum)+"_fine fold" + str(testFold))
+        ##### Create train set for fine
+        data = []
+        partition_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        partition_list.remove(testFold)
+        for x in partition_list:
+            # partition = np.loadtxt("../data/partition_"+str(x))
+            partition = np.asarray(fine_folds[x])
+            if data == []:
+                data = partition
+            else:
+                data = np.vstack((partition, data))
+                # print(str(x)+"=>" +str(data.shape))
+        # print(data.shape)
+        y_train, X_trainPreScale = data[:, 0], data[:, 1:data.shape[1]]
+
+        #### Scale dataset
+        scaler = preprocessing.StandardScaler().fit(X_trainPreScale);
+        X_trainFull = scaler.transform(X_trainPreScale);
+        selector = SelectPercentile(f_classif, percentile=75);
+        selector.fit(X_trainFull, y_train);
+        X_train = selector.transform(X_trainFull);
+
+        ##### Create test set for fine
+        # data_test = np.loadtxt("../data/partition_"+str(testFold))
+        data_test = np.asarray(fine_folds[testFold])
+        y_test, X_testPreScale = data_test[:, 0], data_test[:, 1:data_test.shape[1]];
+        X_testFull = scaler.transform(X_testPreScale);
+        X_test = selector.transform(X_testFull);
+        y_testCoarse = []
+        for i in y_test:
+            if i > 0:
+                y_testCoarse.append(1.)
+            else:
+                y_testCoarse.append(i)
+
+        ##### Train classifier for fine
+        classifier = svm.SVC(C=10.0, kernel='poly', degree=3, probability=True, cache_size=8192,
+                             decision_function_shape='ovr',
+                             verbose=False)
+        clf = classifier.fit(X_train, y_train)
+        joblib.dump(clf, 'fine_models/rnd'+str(rndNum)+'_fine_fold_' + str(testFold) + '.pkl')
+        # clf = joblib.load('fine_models/rnd1_fine_fold_'+str(testFold)+'.pkl')
+        y_pred = clf.predict(X_test);
+
+        ##### Predict test set for fine
+        y_predCoarse = []
+        for i in y_pred:
+            if i > 0:
+                y_predCoarse.append(1.)
+            else:
+                y_predCoarse.append(i)
+        # Compute confusion matrix
+        # cm = confusion_matrix(y_testCoarse, y_predCoarse)
+        # np.set_printoptions(precision=2)
+        # print('Confusion matrix, without normalization')
+        # print(cm)
+        # print('Precision / Recall')
+        # print(precision_score(y_testCoarse, y_predCoarse, average='binary'))
+        # print(recall_score(y_testCoarse, y_predCoarse, average='binary'))
+        y_prob = clf.predict_log_proba(X_test)
+        # print(y_prob.shape)
+        y_score = []
+        for i in range(0, y_prob.shape[0]):
+            # print(y_prob[i,:])
+            maxVal = max(y_prob[i, :])
+            if maxVal == y_prob[i, 0]:
+                maxVal = 0.
+            y_score.append(maxVal - y_prob[i, 0])
+
+        ##### Print this folds roc_auc for fine
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        fpr[1], tpr[1], thresholds = roc_curve(y_testCoarse, y_score)
+        roc_auc[1] = auc(fpr[1], tpr[1])
+        # Plot of a ROC curve
+        plt.figure()
+        plt.plot(fpr[1], tpr[1], label='ROC curve (area = %0.5f)' % roc_auc[1])
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
+        # plt.show()
+        plt.savefig('results/rnd'+str(rndNum)+'_fine_ROC_' + str(testFold) + '.png')
+
+        ##### Print this folds pr_curve for fine
+        precision = dict()
+        recall = dict()
+        average_precision = dict()
+        precision[1], recall[1], _ = precision_recall_curve(y_testCoarse, y_score)
+        average_precision[1] = average_precision_score(y_testCoarse, y_score)
+        # Plot Precision-Recall curve
+        plt.clf()
+        plt.plot(recall[1], precision[1], label='Precision-Recall curve')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.ylim([0.0, 1.05])
+        plt.xlim([0.0, 1.0])
+        plt.title('Precision-Recall: AUC={0:0.5f}'.format(average_precision[1]))
+        plt.legend(loc="lower left")
+        # plt.show()
+        plt.savefig('results/rnd'+str(rndNum)+'_fine_PR_' + str(testFold) + '.png')
+        rnd1_results_fine.append([str(testFold)] + [roc_auc[1]] + [average_precision[1]]);
+
+    ###### Save results to a file
+    f = open('results/rnd'+str(rndNum)+'_fineResults.txt', 'w')
+    for result in rnd1_results_fine:
+        index = 0
+        for r in result[:-1]:
+            f.write(str(result[index]) + ", ")
+            index += 1
+        f.write(str(result[-1]))
+        f.write("\n")
+    f.close()
+
+
+
+
+
+
