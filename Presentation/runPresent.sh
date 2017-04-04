6d1bbfc2eb5269d12d30ace410fee7f2c1f1e4a7
James Duin <jamesdduin@gmail.com>
2017-04-04 16:03:09 -0500

    work for presentation

---
+++ Presentation/runPresent.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+
+#rm -f ThesisPresentation.aux
+#rm -f ThesisPresentation.log
+#rm -f ThesisPresentation.nav
+#rm -f ThesisPresentation.out
+#rm -f ThesisPresentation.snm
+#rm -f ThesisPresentation.toc
+#rm -f ThesisPresentation.vrb
+pdflatex ThesisPresentation
